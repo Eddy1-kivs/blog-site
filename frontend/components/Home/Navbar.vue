f966b50c2331afbeab347735e34c787b38e9de17
josh20122 <jamesjoshua20122@gmail.com>
2024-04-03 00:06:23 +0300

    hcnahes

--- frontend/components/Home/Navbar.vue
+++ frontend/components/Home/Navbar.vue
@@ -1,5 +1,3 @@
-<!-- components/Navbar.vue -->
-
 <template>
   <div class="press-start-text h-full flex flex-col gap-y-4">
     <img src="/image1.jpeg" class="w-full h-32 rounded-b-lg" alt="" />
@@ -10,23 +8,38 @@
       <nuxt-link to="/ppos" class="nav-link" @click="selectTab('PPOS')">PPOS</nuxt-link>
       <nuxt-link to="/auth/login" class="nav-link" @click="selectTab('LOGIN')">LOGIN</nuxt-link>
     </div>
+
+    <div class="flex flex-col md:flex-row text-black font-extrabold text-2xl justify-between cursor-pointer">
+
+      <nuxt-link to="/auth/login" class="nav-link text-sm" v-for="item, index in categories" :key="index"
+        @click="selectTab(item.name)"> {{ item.name }} </nuxt-link>
+
+
+    </div>
     <img src="/image1.jpeg" class="w-full rounded-lg h-32" alt="" />
   </div>
+
+
+
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      selectedTab: "HOME",
-    };
-  },
+<script setup>
+import axios from 'axios';
 
+const selectedTab = ref("HOME");
+const categories = ref([]);
 
-  methods: {
-    selectTab(tab) {
-      this.selectedTab = tab;
-    },
-  },
-};
+const setCategories = () => {
+  axios.get('/all_categories/').then(response => {
+    categories.value = response.data
+  })
+}
+
+onMounted(() => {
+  setCategories()
+})
+
+const selectTab = (tab) => {
+  // this.selectedTab = tab;
+}
 </script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,16 +1,8 @@
 <template>
-  <div
-    class="px-2 bg-[#fde7be] py-2 min-h-screen relative text-gray-900 md:px-20"
-  >
+  <div class="px-2 bg-[#fde7be] py-2 min-h-screen relative text-gray-900 md:px-20">
     <HomeNavbar></HomeNavbar>
-    <div
-      class="w-full grid pt-10 gap-4 md:gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3"
-    >
-      <div
-        class="rounded-sm overflow-hidden"
-        v-for="(item, index) in articles"
-        :key="index"
-      >
+    <div class="w-full grid pt-10 gap-4 md:gap-5 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
+      <div class="rounded-sm overflow-hidden" v-for="(item, index) in articles" :key="index">
         <NuxtLink :to="'/article/' + item.id">
           <img class="w-full rounded-sm" :src="item.cover_photo" alt="" />
           <div class="pt-3">
@@ -51,6 +43,10 @@ const handleFetch = () => {
       });
   }
 };
+
+const handleGetCategories = () => {
+  axios.get('categories')
+}
 
 const handleScroll = () => {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
