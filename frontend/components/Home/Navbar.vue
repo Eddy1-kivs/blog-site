3b357f0d6af9e94395fe2f1275ba6a8d18d7d151
josh20122 <jamesjoshua20122@gmail.com>
2024-04-08 09:32:55 +0300

    chnages

--- frontend/components/Home/Navbar.vue
+++ frontend/components/Home/Navbar.vue
@@ -6,7 +6,11 @@
       <nuxt-link to="/blog" class="nav-link" @click="selectTab('BLOG')">BLOG</nuxt-link>
       <nuxt-link to="/about" class="nav-link" @click="selectTab('ABOUT ME')">ABOUT ME</nuxt-link>
       <nuxt-link to="/ppos" class="nav-link" @click="selectTab('PPOS')">PPOS</nuxt-link>
-      <nuxt-link to="/auth/login" class="nav-link" @click="selectTab('LOGIN')">LOGIN</nuxt-link>
+
+
+      <nuxt-link v-if="!authenticated" to="/auth/login" class="nav-link" @click="selectTab('LOGIN')">LOGIN</nuxt-link>
+      <nuxt-link v-else to="/account/profile" class="nav-link"> {{ user.username }} </nuxt-link>
+
     </div>
 
     <div class="flex flex-col md:flex-row text-black font-extrabold text-2xl justify-between cursor-pointer">
@@ -30,7 +34,8 @@ import { useCategories } from '~/composables/states';
 const selectedTab = ref("HOME");
 const categories = useCategories();
 
-
+const authenticated = useAuthenticated()
+const user = useUser();
 
 const setCategories = () => {
   axios.get('/all_categories/').then(response => {
