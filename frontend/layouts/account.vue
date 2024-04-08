b3400d6b28be5cf6e0c315f2e63fb8bf4fd3ba01
josh20122 <jamesjoshua20122@gmail.com>
2024-04-08 09:24:48 +0300

    chnages

--- frontend/layouts/account.vue
+++ frontend/layouts/account.vue
@@ -3,9 +3,11 @@
     class="px-2 bg-[#fde7be] py-2 min-h-screen pt-4 press-start-text text-gray-900 md:px-20 flex flex-col md:flex-row gap-4 w-full">
     <div class="md:pt-20">
       <div class="px-4 bg-amber-300 py-4 flex flex-col gap-y-2 rounded-md">
-        <!-- <NuxtLink ></NuxtLink> -->
-        <div>Dashboard</div>
-        <div>Profile</div>
+        <nuxt-link to="/account">Dashboard</nuxt-link>
+
+        <nuxt-link to="/account/profile">Profile</nuxt-link>
+
+        <nuxt-link to="/article/create">Create </nuxt-link>
       </div>
     </div>
     <div class=" w-full md:pt-20">
